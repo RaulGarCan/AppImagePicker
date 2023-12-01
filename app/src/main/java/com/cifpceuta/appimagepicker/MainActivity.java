d811d37a7babf959e371f69c6a467d90fd7093d7
Raul <10303@cifpceuta.es>
2023-12-01 03:02:24 +0100

    AppImagePicker terminada

--- app/src/main/java/com/cifpceuta/appimagepicker/MainActivity.java
+++ app/src/main/java/com/cifpceuta/appimagepicker/MainActivity.java
@@ -1,14 +1,48 @@
 package com.cifpceuta.appimagepicker;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imageView;
+    private FloatingActionButton btnFab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        btnFab = findViewById(R.id.btn_fab);
+
+        btnFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePicker(v);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        imageView.setImageURI(uri);
+    }
+
+    public void imagePicker(View v){
+        ImagePicker.with(this)
+                .crop()
+                .compress(1024)
+                .maxResultSize(1080, 1080)
+                .start();
     }
 }
